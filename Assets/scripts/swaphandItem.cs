631675dbdc93a7179581df624d7964230ba109a3
unknown <janko.lilje@gmail.com>
2018-06-18 18:28:45 +0200

    start screen done

--- Assets/scripts/swaphandItem.cs
+++ Assets/scripts/swaphandItem.cs
@@ -6,27 +6,65 @@ public class swaphandItem : MonoBehaviour {
 
 	public GameObject gripper;
 	public GameObject handMirrorprefab;
-	public GameObject currentHandheld;
-	private bool inBackPack = false;
+	public GameObject LightBeamPrefab;
+    public GameObject currentHandheld;
+
+    private GameObject[] levels;
+
+
+    private bool inBackPack = false;
     public bool activeController;
 	private SteamVR_TrackedController controller;
 	private bool handMActive = false;
+    private bool pastmenu = false;
 
-	// Use this for initialization
-	void Awake () {
+
+    // Use this for initialization
+    void Awake () {
 		Vector3 localPos = Vector3.zero;
 		localPos.z = -0.05f;
 		controller = GetComponent<SteamVR_TrackedController>();
-		currentHandheld = Instantiate(gripper, Vector3.zero, Quaternion.LookRotation(Vector3.zero));
-		currentHandheld.transform.parent = gameObject.transform;
-		currentHandheld.transform.localPosition = localPos;
-		currentHandheld.transform.localRotation = Quaternion.Euler (180,-90,-90);
-	}
+
+
+        levels = GameObject.FindGameObjectsWithTag("LevelControl");
+        if (LightBeamPrefab && levels[0].gameObject.GetComponent<levels>().currlvl == 0)
+        {
+
+            currentHandheld = Instantiate(LightBeamPrefab, Vector3.zero, Quaternion.LookRotation(Vector3.zero));
+
+            currentHandheld.transform.parent = gameObject.transform;
+            currentHandheld.transform.localPosition = localPos;
+            currentHandheld.transform.localRotation = Quaternion.Euler(0, -90, 90);
+        }
+        else
+        {
+            currentHandheld = Instantiate(gripper, Vector3.zero, Quaternion.LookRotation(Vector3.zero));
+            currentHandheld.transform.parent = gameObject.transform;
+            currentHandheld.transform.localPosition = localPos;
+            currentHandheld.transform.localRotation = Quaternion.Euler(180, -90, -90);
+        }
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
         //Debug.Log(inBackPack);
+        if(!pastmenu && levels[0].gameObject.GetComponent<levels>().currlvl > 0)
+        {
+            Destroy(currentHandheld);
+            Vector3 localPos = Vector3.zero;
+            localPos.z = -0.05f;
+
+            currentHandheld = Instantiate(gripper, Vector3.zero, Quaternion.identity);
+            currentHandheld.transform.parent = gameObject.transform;
+            currentHandheld.transform.localPosition = localPos;
+            currentHandheld.transform.localRotation = Quaternion.Euler(180, -90, -90);
+
+            Collider m_ObjectCollider;
+            m_ObjectCollider = GetComponent<Collider>();
+            m_ObjectCollider.enabled = true;
+            pastmenu = true;
+        }
 
 
     }
@@ -44,11 +82,7 @@ public class swaphandItem : MonoBehaviour {
 
 	private void Triggerpull(object sender, ClickedEventArgs e)
 	{
-        GameObject[] hold = GameObject.FindGameObjectsWithTag("LevelControl");
-        if (hold[0].gameObject.GetComponent<levels>().currlvl == 0)
-        {
-            hold[0].gameObject.GetComponent<levels>().currComplete++;
-        }
+       
 
         if (inBackPack && activeController)
 		{
